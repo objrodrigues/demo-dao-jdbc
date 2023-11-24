e8c628e87eb78cba550eedf8ef2186c78122cd5a
Diego Rodrigues <objrodrigues@gmail.com>
2023-11-24 10:16:27 -0300

    Departament entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Departament obj = new Departament(1, "books");
+		System.out.println(obj);
+	}
+}
