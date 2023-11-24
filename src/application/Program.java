b0bbed621cdf990488b74c0cfabfa1cb28b91732
Diego Rodrigues <objrodrigues@gmail.com>
2023-11-24 16:24:27 -0300

    DepartmentDao implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -43,13 +43,13 @@ public class Program {
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
-		System.out.println("Update completed"); */
+		System.out.println("Update completed"); 
 		
 		System.out.println("===== TEST 6: seller delete =====");
 		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
-		System.out.println("Delete completed"); 
+		System.out.println("Delete completed"); */
 		
 		
 		sc.close();
