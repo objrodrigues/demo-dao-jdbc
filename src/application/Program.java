fdb08bfe49433efb9b25f7dc2884567ee34aa0b7
Diego Rodrigues <objrodrigues@gmail.com>
2023-11-24 11:02:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
 		Departament obj = new Departament(1, "books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@mail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 }
